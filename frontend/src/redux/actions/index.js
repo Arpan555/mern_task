cfb51461d80c208bd114ea03ce25f8eab61e1b90
Arpan Patidar <58987469+Arpan555@users.noreply.github.com>
2021-08-18 18:00:00 +0530

    Delete frontend directory

--- frontend/src/redux/actions/index.js
+++
@@ -1,7 +0,0 @@
-export const CREATE = 'CREATE';
-export const UPDATE = 'UPDATE';
-export const DELETE = 'DELETE';
-export const FETCH_ALL = 'FETCH_ALL';
-export const SET_USER="SET_USER"
-export const SIGNIN="SIGNIN"
-export const SIGNUP="SIGNUP"
