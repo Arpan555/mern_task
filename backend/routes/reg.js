5ed78801db63ea84f7b7b14dc965b106961156b0
Arpan Patidar <58987469+Arpan555@users.noreply.github.com>
2021-08-18 17:59:45 +0530

    Delete backend directory

--- backend/routes/reg.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const {signup,signin}=require("../controllers/reg.js")
-
-const reg_router=express.Router()
-
-reg_router.post("/signup",signup)
-reg_router.post("/signin",signin)
-
-module.exports=reg_router
