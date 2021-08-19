fb08883f20785b0263d5eb97a3df0c7729b298c8
Arpan Patidar <58987469+Arpan555@users.noreply.github.com>
2021-08-19 22:15:04 +0530

    Delete backend directory

--- backend/routes/post.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const {addPost,showPosts,myPost}=require("../controllers/post.js")
-
-const post_router=express.Router()
-
-post_router.post("/addpost",addPost)
-post_router.get("/showposts",showPosts)
-post_router.get("/mypost/:id",myPost)
-module.exports=post_router
