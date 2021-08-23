ec0b7295edfcb988dff3cc12ace625d9c6daca33
Arpan Patidar <58987469+Arpan555@users.noreply.github.com>
2021-08-23 09:34:27 +0530

    Delete backend directory

--- backend/routes/comment.js
+++
@@ -1,7 +0,0 @@
-const express=require("express")
-const {getComments,addComment}=require("../controllers/comment.js")
-const comment_router=express.Router()
-
-comment_router.get("/showcomments/:id",getComments)
-comment_router.post("/addcomment",addComment)
-module.exports=comment_router
--- backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const {createUser,getUsers,deleteUser,updateUser}=require("../controllers/users.js")
-const users_router=express.Router()
-
-users_router.post("/adduser",createUser);
-users_router.get("/users",getUsers);
-users_router.delete("/delete/:id",deleteUser);
-users_router.patch("/edit/:id",updateUser);
-module.exports=users_router
