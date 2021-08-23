4489b7e5e94b4174b94b9690af16948c7f6724a6
Arpan Patidar <58987469+Arpan555@users.noreply.github.com>
2021-08-23 13:34:36 +0530

    Delete frontend directory

--- frontend/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from "redux"
-import regReducer from "./regReducer"
-import userReducer from "./userReducer"
-import postReducer from "./postReducer"
-export const reducers =combineReducers({
-    regReducer,userReducer,postReducer
-})
